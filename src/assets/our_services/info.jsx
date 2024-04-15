6655492fad58aca3dc1ccad5e4d58a8b94a00a76
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-04-15 16:57:59 -0300

    FEAT: add consulting description

--- src/assets/our_services/info.jsx
+++ src/assets/our_services/info.jsx
@@ -1,3 +1,47 @@
-import { landingVector,mobileVector,webAppVector,uiUxDesingVector,apiResstVector,consultancyVector} from "./iconsSVG"
+import {
+  apiResstVector,
+  consultancyVector,
+  landingVector,
+  mobileVector,
+  uiUxDesingVector,
+  webAppVector,
+} from "./iconsSVG";
 
-export const servicesArray=[{title:"Landing page",description:"Creamos páginas de destino impactantes y efectivas que convierten a los visitantes en clientes potenciales ofreciendo una experiencia intuitiva y atractiva. Nuestros diseños optimizados destacan los productos y servicios de las empresas de forma eficaz.",image:landingVector},{title:"Web Apps",description:"Desarrollamos aplicaciones web personalizadas y escalables que ofrecen funcionalidades específicas a través de navegadores, adaptándose a diversas plataformas y dispositivos. Nuestras soluciones garantizan una experiencia consistente y de calidad para los usuarios.",image:webAppVector},{title:"Mobile Apps",description:"Creamos aplicaciones móviles nativas para iOS y Android, garantizando un rendimiento óptimo y una experiencia de usuario excepcional. Nuestras soluciones aprovechan las características únicas de cada plataforma para ofrecer funcionalidad avanzada y una interfaz intuitiva.",image:mobileVector},{title:"API Rest",description:"Creamos API RESTful para facilitar la comunicación entre sistemas, ofreciendo acceso seguro a datos y funcionalidades. Garantizamos escalabilidad, seguridad y rendimiento para satisfacer las necesidades de integración de nuestros clientes.",image:apiResstVector},{title:"Diseño UX/UI",description:"Realizamos el diseño de interfaces intuitivas y atractivas, enfocadas en optimizar la usabilidad y satisfacción del usuario. Con un compromiso con la excelencia estética y funcional, creamos diseños que cautivan y mejoran la percepción del producto o servicio.",image:uiUxDesingVector},{title:"Consultoría",description:"Brindamos asesoramiento tecnico",image:consultancyVector}]
+export const servicesArray = [
+  {
+    title: "Landing page",
+    description:
+      "Creamos páginas de destino impactantes y efectivas que convierten a los visitantes en clientes potenciales ofreciendo una experiencia intuitiva y atractiva. Nuestros diseños optimizados destacan los productos y servicios de las empresas de forma eficaz.",
+    image: landingVector,
+  },
+  {
+    title: "Web Apps",
+    description:
+      "Desarrollamos aplicaciones web personalizadas y escalables que ofrecen funcionalidades específicas a través de navegadores, adaptándose a diversas plataformas y dispositivos. Nuestras soluciones garantizan una experiencia consistente y de calidad para los usuarios.",
+    image: webAppVector,
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Creamos aplicaciones móviles nativas para iOS y Android, garantizando un rendimiento óptimo y una experiencia de usuario excepcional. Nuestras soluciones aprovechan las características únicas de cada plataforma para ofrecer funcionalidad avanzada y una interfaz intuitiva.",
+    image: mobileVector,
+  },
+  {
+    title: "API Rest",
+    description:
+      "Creamos API RESTful para facilitar la comunicación entre sistemas, ofreciendo acceso seguro a datos y funcionalidades. Garantizamos escalabilidad, seguridad y rendimiento para satisfacer las necesidades de integración de nuestros clientes.",
+    image: apiResstVector,
+  },
+  {
+    title: "Diseño UX/UI",
+    description:
+      "Realizamos el diseño de interfaces intuitivas y atractivas, enfocadas en optimizar la usabilidad y satisfacción del usuario. Con un compromiso con la excelencia estética y funcional, creamos diseños que cautivan y mejoran la percepción del producto o servicio.",
+    image: uiUxDesingVector,
+  },
+  {
+    title: "Consultoría",
+    description:
+      "Como consultora en tecnologías de la información, nos especializamos en brindar asesoramiento técnico integral a nuestros clientes. Nuestro equipo de expertos en IT está dedicado a proporcionar soluciones personalizadas que optimicen el rendimiento, la seguridad y la eficiencia de los sistemas informáticos de su empresa. Ya sea en la implementación de nuevas tecnologías, la resolución de problemas o la optimización de procesos, estamos comprometidos a ofrecerle el soporte técnico que necesita para alcanzar sus objetivos empresariales.",
+    image: consultancyVector,
+  },
+];
