0d70fcdf8441a04dd00c6b07d915e9ad123fe35e
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-04-16 18:36:22 -0300

    FEAT: update phone number

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -1,42 +1,67 @@
 import { AiOutlineMail } from "react-icons/ai";
 import { BsXLg } from "react-icons/bs";
-import { FaLinkedinIn,FaInstagram,FaWhatsapp,FaBars } from "react-icons/fa";
-import { IoIosArrowBack,IoIosArrowForward,IoMdArrowDropleftCircle,IoMdArrowDroprightCircle  } from "react-icons/io";
+import { FaBars, FaInstagram, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoMdArrowDropleftCircle,
+  IoMdArrowDroprightCircle,
+} from "react-icons/io";
 
+const email = <AiOutlineMail title="Email" />;
+const phone = <FaWhatsapp title="Phone" />;
+const linkedin = <FaLinkedinIn title="Linkedin" />;
+const instagram = <FaInstagram title="Instagram" />;
 
-const email=(<AiOutlineMail title="Email" />)
-const phone=(<FaWhatsapp title="Phone"/>)
-const linkedin=(<FaLinkedinIn title="Linkedin" />)
-const instagram=(<FaInstagram title="Instagram"/>)
+export const socialMedia = [
+  { icon: email, link: "mailto:info@zmdevs.com", text: "info@zmdevs.com" },
+  {
+    icon: phone,
+    link: "https://wa.me/5493425906598",
+    text: "+54-93425906598",
+  },
+  {
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/czaburlin/",
+    text: "zmdevs.linkedin",
+  },
+  {
+    icon: instagram,
+    link: "https://www.instagram.com/zmdevs/",
+    text: "zmDevs",
+  },
+];
 
+const arrowLeft = <IoIosArrowBack />;
+const arrowRight = <IoIosArrowForward />;
+const circleArrowsLeft = <IoMdArrowDropleftCircle />;
+const circleArrowsRight = <IoMdArrowDroprightCircle />;
 
-export const socialMedia=[
-    {icon:email,link:"mailto:franmack556@gmail.com",text:"info@zmdevs.com"},
-    {icon:phone,link:"https://www.instagram.com/nuggets/",text:"+54-9342-4564569"},
-    {icon:linkedin,link:"https://www.instagram.com/nuggets/",text:"zmdevs.linkedin"},
-    {icon:instagram,link:"https://www.instagram.com/nuggets/",text:"zmDevs"},
-]
+export const arrows = {
+  left: arrowLeft,
+  right: arrowRight,
+};
 
+export const circleArrows = {
+  left: circleArrowsLeft,
+  right: circleArrowsRight,
+};
 
-const arrowLeft=(<IoIosArrowBack />)
-const arrowRight=(<IoIosArrowForward />)
-const circleArrowsLeft=(<IoMdArrowDropleftCircle />)
-const circleArrowsRight=(<IoMdArrowDroprightCircle />)
+export const HamburguerMenu = ({ handleShowMobileMenu }) => {
+  return (
+    <FaBars
+      id="menu-hamburguesa-icon"
+      onClick={handleShowMobileMenu}
+      style={{ marginRight: "5%" }}
+    />
+  );
+};
 
-export const arrows={
-    left:arrowLeft,
-    right:arrowRight
-}
-
-export const circleArrows={
-    left:circleArrowsLeft,
-    right:circleArrowsRight
-} 
-
-
-export const HamburguerMenu=({handleShowMobileMenu})=>{
-return(<FaBars id="menu-hamburguesa-icon" onClick={handleShowMobileMenu} style={{marginRight:"5%"}}/>)}
-
-export const ExitIcon=({handleShowMobileMenu})=>{
-    return(<BsXLg onClick={handleShowMobileMenu} style={{marginLeft:"5%",marginTop:"5%"}}/>)}
-
+export const ExitIcon = ({ handleShowMobileMenu }) => {
+  return (
+    <BsXLg
+      onClick={handleShowMobileMenu}
+      style={{ marginLeft: "5%", marginTop: "5%" }}
+    />
+  );
+};
