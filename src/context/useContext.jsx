81b99a9ce9cdecbc7502b2fc5fdb3931d6254b2c
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-07-04 09:56:50 -0300

    FEAT: set english as default language

--- src/context/useContext.jsx
+++ src/context/useContext.jsx
@@ -1,26 +1,28 @@
 import React, { createContext, useState } from "react";
 
-const initialLanguage = "spanish";
+const initialLanguage = "english";
 
 const LanguageContext = createContext();
 
 const LanguageContextProvider = ({ children }) => {
-    const [language, setLanguage] = useState(initialLanguage);
+  const [language, setLanguage] = useState(initialLanguage);
 
-    const toggleLanguage = () => {
-        setLanguage(prevLanguage => prevLanguage === "spanish" ? "english" : "spanish");
-    }
-
-    const value = {
-        language,
-        toggleLanguage
-    }
-
-    return (
-        <LanguageContext.Provider value={value}>
-            {children}
-        </LanguageContext.Provider>
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) =>
+      prevLanguage === "spanish" ? "english" : "spanish"
     );
-}
+  };
+
+  const value = {
+    language,
+    toggleLanguage,
+  };
+
+  return (
+    <LanguageContext.Provider value={value}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
 
 export { LanguageContext, LanguageContextProvider };
